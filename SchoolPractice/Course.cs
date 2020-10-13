abe55733985f17a801a8a16bb5e34339f49a7242
Megan Orton <megan.orton0@gmail.com>
2020-10-12 20:18:50 -0500

    created course and teacher classes

---
+++ SchoolPractice/Course.cs
@@ -0,0 +1,12 @@
+using System.Collections.Generic;
+
+namespace SchoolPractice
+{
+    public class Course
+    {
+        public string subject;
+        public string teacher;
+        public int credits;
+        public List<string> students;
+    }
+}
---
+++ Teacher/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Security.Cryptography.X509Certificates;
+
+namespace Teacher
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            public string FirstName { get; set; }
+            public string LastName { get; set; }
+            public string Subject { get; set; }
+            public int YearsTeaching { get; set; }
+        }
+    }
+}
